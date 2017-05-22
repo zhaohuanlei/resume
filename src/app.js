3e4dd195c29879a6fc3db612979e76c0a14038fc
huanlei <zhaoxiaolei@outlook.com>
2017-05-22 20:14:32 +0800

    提交

--- src/app.js
+++ src/app.js
@@ -2,16 +2,10 @@
  * @Author: zhaohuanlei
  * @Date: 2017-05-18 18:30:11
  * @Last Modified by: zhaohuanlei
- * @Last Modified time: 2017-05-19 18:49:27
+ * @Last Modified time: 2017-05-22 20:13:36
  */
 
 import app from "app.scss";
-
 import Vue from "vue";
 
-function sum (a = 1, b = 2) {
-    return a + b;
-}
-
-var n = sum(3);
-console.log(n);
+Vue.use(AwesomeSwiper);
